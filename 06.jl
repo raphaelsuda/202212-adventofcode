0591d3d603f0cb2f8158c4e23953ecb7b08531dc
Raphael Reismueller <raphael@reismueller.at>
2022-12-06 09:01:05 +0100

    Day 6

---
+++ 06.jl
@@ -0,0 +1,9 @@
+signal = read("06.input", String)
+
+find_marker(n, signal) = findfirst(map(i->length(unique(signal[i:i+(n-1)])) == n, 1:length(signal)-(n+1))) + (n-1)
+
+## PART 1
+find_marker(4, signal)
+
+## PART 2
+find_marker(14, signal)
